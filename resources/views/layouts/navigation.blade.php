61e6920298e7dd815b3d760b67637b959ab78f5b
Sebas21EC <sebastiantabango@gmail.com>
2023-07-12 16:31:48 -0500

    Se agrega  opcion de employyes en barra de navegación

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,6 +38,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('employee.index')" :active="request()->routeIs('index')">
+                        {{ __('Empleados') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
