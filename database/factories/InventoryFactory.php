1bbe5513cb9e6f5bbf177607fff5c12a197544b1
Sebas21EC <sebastiantabango@gmail.com>
2023-07-19 00:17:57 -0500

    Cambio en seeder inventory

--- database/factories/InventoryFactory.php
+++ database/factories/InventoryFactory.php
@@ -21,16 +21,25 @@ class InventoryFactory extends Factory
      */
     public function definition()
     {
+        // $table->id();
+        // $table->string('machine');
+        // $table->string('detail');
+        // $table->integer('quantity');
+        // $table->decimal('unit_price', 8, 2);
+        // $table->decimal('total_price', 8, 2);
+        // $table->string('state');
+        // $table->softDeletes();
+        // $table->timestamps();
         return [
-            'machine' => $this->faker->word,
-        'detail' => $this->faker->word,
-        'quantity' => $this->faker->randomDigitNotNull,
-        'unit_price' => $this->faker->randomDigitNotNull,
-        'total_price' => $this->faker->randomDigitNotNull,
-        'state' => $this->faker->word,
-        'deleted_at' => $this->faker->date('Y-m-d H:i:s'),
-        'created_at' => $this->faker->date('Y-m-d H:i:s'),
-        'updated_at' => $this->faker->date('Y-m-d H:i:s')
+         'machine' => $this->faker->word,
+            'detail' => $this->faker->word,
+            'quantity' => $this->faker->randomDigitNotNull,
+            'unit_price' => $this->faker->randomFloat(2, 0, 999999.99),
+            'total_price' => $this->faker->randomFloat(2, 0, 999999.99),
+            'state' => $this->faker->word,
+            'created_at' => $this->faker->date('Y-m-d H:i:s'),
+            'updated_at' => $this->faker->date('Y-m-d H:i:s')
+            
         ];
     }
 }
