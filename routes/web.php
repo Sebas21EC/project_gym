220ab681874750ff0b13c1bb9affe3e074c666ec
VivianaCuaspa <vacuaspab@utn.edu.ec>
2023-07-19 11:36:37 -0500

    change routes

--- routes/web.php
+++ routes/web.php
@@ -70,12 +70,8 @@ Route::get('audit_trail_user_actions', [\App\Http\Controllers\audit_trail\AuditS
 
 
 
-<<<<<<< Updated upstream
 Route::resource('healthCards', App\Http\Controllers\HealthCardController::class)->middleware('auth');
-=======
-Route::resource('healthCards', App\Http\Controllers\HealthCardController::class);
-Route::get('payment_master_detail{id}', 'App\Http\Controllers\PaymentController@getMasterDetail')->name('payments.master_detail');
 
->>>>>>> Stashed changes
+Route::get('payment_master_detail{id}', 'App\Http\Controllers\PaymentController@getMasterDetail')->name('payments.master_detail');
 
 require __DIR__.'/auth.php';
