ffb599c7b5eaa9c0af611b3473557317a9f91d40
Sebas21EC <sebastiantabango@gmail.com>
2023-07-19 11:42:48 -0500

    Camios en controller Partner

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\CreatePartnerRequest;
 use App\Http\Requests\UpdatePartnerRequest;
 use App\Repositories\PartnerRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Response;
 
 class PartnerController extends Controller
@@ -27,7 +29,15 @@ class PartnerController extends Controller
      */
     public function index(Request $request)
     {
+
+        $rol_names = array("administrator", "operator", "user");
+        if (!Gate::allows('has_role', [$rol_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_index_partner'], '');
+            return redirect()->route('dashboard')->with('error', 'Usted no tiene permiso!');
+        }
         $partners = $this->partnerRepository->all();
+
+        $this->addAudit(Auth::user(), $this->typeAudit['access_index_partner'], '');
         return view('partners.index')
             ->with('partners', $partners);
     }
@@ -40,6 +50,13 @@ class PartnerController extends Controller
     public function create()
     {
 
+        $rol_names = array( "operator", "user");
+        if (!Gate::allows('has_role', [$rol_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_create_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+
+        $this->addAudit(Auth::user(), $this->typeAudit['access_create_partner'], '');
         return view('partners.create');
     }
 
@@ -52,9 +69,20 @@ class PartnerController extends Controller
      */
     public function store(CreatePartnerRequest $request)
     {
+        $role_names = array("operator", "user");
+        if (!Gate::allows('has_role', [$role_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_store_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+
         $input = $request->all();
 
         $partner = $this->partnerRepository->create($input);
+
+        $data_new =json_encode($partner);
+
+        $this->addAudit(Auth::user(), $this->typeAudit['access_store_partner'],'', $data_new); 
+
         return redirect()->route('partners.index')->with('success', 'Clienta creada con éxito');
     }
 
@@ -67,6 +95,13 @@ class PartnerController extends Controller
      */
     public function show($id)
     {
+
+        $rol_names = array("administrator", "operator", "user");
+        if (!Gate::allows('has_role', [$rol_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_show_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+
         $partner = $this->partnerRepository->find($id);
 
         if (empty($partner)) {
@@ -87,6 +122,12 @@ class PartnerController extends Controller
      */
     public function edit($id)
     {
+        $role_names = array("operator", "user");
+        if (!Gate::allows('has_role', [$role_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_edit_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+
         $partner = $this->partnerRepository->find($id);
 
         if (empty($partner)) {
@@ -94,6 +135,7 @@ class PartnerController extends Controller
 
             return redirect(route('partners.index'));
         }
+        $this->addAudit(Auth::user(), $this->typeAudit['access_edit_partner'], '');
 
         return view('partners.edit')->with('partner', $partner);
     }
@@ -108,6 +150,13 @@ class PartnerController extends Controller
      */
     public function update($id, UpdatePartnerRequest $request)
     {
+        $role_names = array("operator", "user");
+        if (!Gate::allows('has_role', [$role_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_update_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+        $data_old = json_encode($this->partnerRepository->find($id));
+
         $partner = $this->partnerRepository->find($id);
 
         if (empty($partner)) {
@@ -117,6 +166,10 @@ class PartnerController extends Controller
         }
 
         $partner = $this->partnerRepository->update($request->all(), $id);
+
+        $data_new = json_encode($partner);
+
+        $this->addAudit(Auth::user(), $this->typeAudit['access_update_partner'], $data_old, $data_new);
 
         return redirect()->route('partners.index')->with('success', 'Clienta actualizado con éxito');
     }
@@ -132,6 +185,13 @@ class PartnerController extends Controller
      */
     public function destroy($id)
     {
+
+        $role_names = array("operator");
+        if (!Gate::allows('has_role', [$role_names])) {
+            $this->addAudit(Auth::user(), $this->typeAudit['not_access_destroy_partner'], '');
+            return redirect()->route('partners.index')->with('error', 'Usted no tiene permiso!');
+        }
+
         $partner = $this->partnerRepository->find($id);
 
         if (empty($partner)) {
@@ -143,6 +203,10 @@ class PartnerController extends Controller
         $this->partnerRepository->delete($id);
 
         // Flash::success('Partner deleted successfully.');
+
+        $data_old = json_encode($partner);
+
+        $this->addAudit(Auth::user(), $this->typeAudit['access_destroy_partner'], $data_old, '');
 
         return redirect()->back()->with('success', 'Clienta eliminada con éxito');
     }
